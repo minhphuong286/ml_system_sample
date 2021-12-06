5e9983b90ac88ec2a3a482005afc386ea674bebc
minhphuong286 <minhphuongbigboss280@gmail.com>
2021-12-06 14:52:13 +0700

    add ml code

---
+++ backend/server/server/.ipynb_checkpoints/urls-checkpoint.py
@@ -0,0 +1,35 @@
+"""server URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
+from app.views import EndpointViewSet
+from app.views import MLModelViewSet
+from app.views import MLModelStatusViewSet
+from app.views import MLRequestViewSet
+
+router = DefaultRouter(trailing_slash=False)
+router.register(r"endpoints", EndpointViewSet, basename="endpoints")
+router.register(r"mlmodels", MLModelViewSet, basename="mlmodels")
+router.register(r"mlmodelstatuses", MLModelStatusViewSet,
+                basename="mlmodelstatuses")
+router.register(r"mlrequests", MLRequestViewSet, basename="mlrequests")
+
+urlpatterns = [
+    url(r"^api/v1/", include(router.urls)),
+    path('admin/', admin.site.urls),
+]
--- backend/server/server/urls.py
+++ backend/server/server/urls.py
@@ -15,7 +15,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
+from app.views import EndpointViewSet
+from app.views import MLModelViewSet
+from app.views import MLModelStatusViewSet
+from app.views import MLRequestViewSet
+
+router = DefaultRouter(trailing_slash=False)
+router.register(r"endpoints", EndpointViewSet, basename="endpoints")
+router.register(r"mlmodels", MLModelViewSet, basename="mlmodels")
+router.register(r"mlmodelstatuses", MLModelStatusViewSet,
+                basename="mlmodelstatuses")
+router.register(r"mlrequests", MLRequestViewSet, basename="mlrequests")
 
 urlpatterns = [
+    url(r"^api/v1/", include(router.urls)),
     path('admin/', admin.site.urls),
 ]
